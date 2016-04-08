15252eda3f9efeac4b71331bc7a808b94a2e8e01
dmatiasr <matu_dmr@hotmail.com>
2016-04-08 16:24:57 -0300

    inversiooon

--- Inversion/Inversion.java
+++ Inversion/Inversion.java
@@ -11,38 +11,31 @@ public class Inversion{
 		
 		int count=0;
 		System.out.println("RESULTADO"+contarInversion(array,count));
-		/*System.out.println("VALOR DEL ULTIMO "+array[array.length-1]);
-		int begin= 0;
-		int end= array.length;
-		double[] array1 = copyArray(array,begin,(end/2));
-		double[] array2 = copyArray(array,(end/2),end);
-		System.out.println("PRIMER ARREGLO CARGADO CON");
-		for (int i=0; i<array1.length;i++){
-			System.out.println(""+array1[i]);
-		}
-		System.out.println("SEGUNDO ARREGLO CARGADO CON");
-		for (int i=0; i<array2.length;i++){
-			System.out.println(""+array2[i]);
-		}
-	*/	
+		
 	}
-	public static int contarInversion(double[] array,int count){
+	public static void inversion( double array ){
 		int begin= 0;
 		int end= array.length;
 		double[] array1 = copyArray(array,begin,(end/2));
 		double[] array2 = copyArray(array,(end/2),end);
-		if (array1!=null&&array2!=null){ 
-			for (int i=0;i<array2.length;i++){
-				if (array1[array1.length-1] > array2[i]){
-					count++;
-				}
+		int count=0;
+		inversion(array1);
+		inversion(array2);
+		int result= contarInversion(array1,count)+contarInversion(array2,count)
+	}
+
+	public static int contarInversion(double[] array1,int count){
+		if (array1==null || array1.length=1){
+			return 0
+		}else 
+			if (array1[0]>array1[1]){
+				count++;
 			}
-			count+=contarInversion(array1,count)+contarInversion(array1,count);
-	 }
+		}	
+
 		return count;
 	}
-	
-
+	//Copia los elementos de un array a otro.
 	public static double[] copyArray(double[] array,int begin,int end){
     	int cota= end-begin;
     	double[] aux= new double[cota];
