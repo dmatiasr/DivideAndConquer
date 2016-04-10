d29df58da271e46abe3c13fdcf16adf74d7f3f0c
dmatiasr <matu_dmr@hotmail.com>
2016-04-10 18:10:39 -0300

    Ejercicio Suma minima

---
+++ SumaMinimaSubsecuencia/SumaMinimaSecuencia.java
@@ -0,0 +1,12 @@
+public class SumaMinimaSecuencia {
+	public static void main (String[] args){
+		float[] array= new float[5];
+		array[0]= 3;
+		array[1]= 10;
+		array[2]= 2;
+		array[3]= 9;
+		array[4]= 5;
+
+	}	
+
+}
