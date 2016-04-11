a9f9cbe50238c1d218ea1706cc40f58e8bee0bb5
MauriDV <mdellevedove12@gmail.com>
2016-04-11 16:58:30 -0300

    caso base retorna 0, modificacion de minValorDeTres para que se adapte al caso base

--- SumaMinimaSubsecuencia/SumaMinimaSecuencia.java
+++ SumaMinimaSubsecuencia/SumaMinimaSecuencia.java
@@ -1,12 +1,14 @@
+import java.util.*;
 public class SumaMinimaSecuencia {
 	public static void main (String[] args){
+		LinkedList<Integer> lista=new LinkedList<Integer> ();
 		int[] array= new int[5];
-		array[0]= 3;
-		array[1]= 10;
-		array[2]= 9;
-		array[3]= 2;
-		array[4]= 5;
-		int res=sumaMinima(array,0,(array.length)-1);
+		array[0]=-3;
+		array[1]=10;
+		array[2]=-9;
+		array[3]=3;
+		array[4]=-5;
+		int res = sumaMinima(array,0,(array.length)-1);
 		System.out.println("Suma "+ res);
 	}	
 	public static int sumaDelMedio(int[] array, int left, int middle, int right){
@@ -17,7 +19,6 @@ public class SumaMinimaSecuencia {
 			suma+=array[i];
 			if(suma<sumaMinLeft){
 				sumaMinLeft=suma;
-				System.out.println("sumo left"+sumaMinLeft);
 			}
 		}
 		//ver del medio a derecha la secuencia de suma minima
@@ -27,7 +28,6 @@ public class SumaMinimaSecuencia {
 			suma+=array[i];
 			if(suma<sumaMinRight){
 				sumaMinRight=suma;
-				System.out.println("sumo right"+sumaMinRight);
 			}
 		}
 		return sumaMinLeft+sumaMinRight;			
@@ -39,25 +39,28 @@ public class SumaMinimaSecuencia {
 		}
 		//caso base si tiene un solo elemento el arreglo
 		if(left==right){
-			int valMax=Integer.MAX_VALUE;
-			return (valMax);
+			return (0);
 		}else{
 			int middle= (left+right)/2;
 			return minValorDeTres(sumaMinima(array,left,middle),sumaMinima(array,middle+1,right),sumaDelMedio(array,left,middle,right));
 		}
 		
 	}
-	//Calcula el max de tres valores
+	//Calcula el min de tres valores
 	public static int minValorDeTres(int a, int b, int c){
 		int min=0;
-		if (a<b && a<c){
-			min = a;
-		}
-		if (b<a && b<c){
-			min = b;
-		}
-		if (c<a && c<b){
+		if(a==0||b==0){
 			min = c;
+		}else{
+			if (a<=b && a<=c){
+				min = a;
+			}
+			if (b<=a && b<=c){
+				min = b;
+			}
+			if (c<=a && c<=b){
+				min = c;
+			}
 		}
 		return min;
 	}
